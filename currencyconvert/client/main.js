7edab979da82071458d1906f1a3b5351de4fb596
whoopseez <whoopseez@gmail.com>
2017-07-11 22:01:32 -0400

    working in console

--- currencyconvert/client/main.js
+++ currencyconvert/client/main.js
@@ -1,22 +1,34 @@
-var HttpClient = function () {
-	this.get = function (aUrl, aCallback) {
-		var anHttpRequest = new XMLHttpRequest();
-		anHttpRequest.onreadystatechange = function () {
-			if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-				aCallback(anHttpRequest.responseText);
+	var HttpClient = function () {
+		this.get = function (aUrl, aCallback) {
+			var anHttpRequest = new XMLHttpRequest();
+			anHttpRequest.onreadystatechange = function () {
+				if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+					aCallback(anHttpRequest.responseText);
+			}
+
+			anHttpRequest.open("GET", aUrl, true);
+			anHttpRequest.send(null);
 		}
-
-		anHttpRequest.open("GET", aUrl, true);
-		anHttpRequest.send(null);
 	}
-}
 
-var client = new HttpClient();
-client.get('https://api.fixer.io/latest?base=USD', function (response) {
-	currency = JSON.parse(response);
-	//console.log(currency);
 
-	var output = document.getElementById('output');
-	console.log(currency);
-	output.innerHTML = currency.rates.EUR;
-});
+	Template.app.events({
+		"click button" (event, instance) {
+
+			var client = new HttpClient();
+			client.get('https://api.fixer.io/latest?base=USD', function (response) {
+				currency = JSON.parse(response);
+				//console.log(currency);
+
+				var output = document.getElementById('output');
+				console.log(output);
+
+				var convfrom = instance.$("#js-from").val();
+				//console.log(convfrom);
+				//console.dir($.type(currency.rates.EUR));
+
+				output.innerHTML = parseFloat(currency.rates.EUR) * convfrom;
+			});
+		},
+
+	})
